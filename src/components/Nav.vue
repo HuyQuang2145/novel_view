4806f082dee3df4e893eec2d366222e848f67ba9
HuyQuang2145 <es21hn@gmail.com>
2021-07-28 21:49:33 +0700

    Add Bootstrap

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,19 @@
 <template>
-  <nav>
-    <div class="menu-item"><router-link to="./Home">Home</router-link></div>
-    <div class="menu-item"><router-link to="./Products">List</router-link></div>
-    <Dropdown title="Genres" :items="genres" />
-    <div class="menu-item"><router-link to="./About">About</router-link></div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item menu-item">
+        <router-link to="./Home">Home</router-link>
+      </li>
+      <li class="nav-item menu-item">
+        <router-link to="./Products">List</router-link>
+      </li>
+      <Dropdown title="Genres" :items="genres" />
+      <li class="nav-item menu-item">
+        <router-link to="./About">About</router-link>
+      </li>
+    </ul>
+  </nav>
   </nav>
 </template>
 
